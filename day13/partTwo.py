99e79ecbfed345f3c78411ceb4ba9eb5d7ed9b4a
Hector-bit <hectormartinez.2978@gmail.com>
2021-05-21 21:53:31 -0700

    parttow

--- day13/partTwo.py
+++ day13/partTwo.py
@@ -1,43 +1,29 @@
 from os import terminal_size
 
 #BELOW IS PROBLEM DATA
-time = 1002632
+time = 1000002632
 arrival = 1002632
 #BELOW IS TEST DATA
 # time = 939
 # arrival = 939
-travel_by = 829
 
-def doubleCheck(arr, time):
+def check(arr, time):
     for i in arr:
-        if i is not '':
+        if i != '':
             if time % int(i) != 0:
-                print("This time did not work: ", time)
                 return False
+        time += 1
     return True
 
-def checkIfWorks(arr, time):
-    if (time - 23) % 23 == 0:
-        print(time, "<== TIME")
-        if doubleCheck(arr, time):
-            return True
-        return False
-
-
-def partTwo(arr):
-    global time
-    global travel_by
-    while True:
-        if time % travel_by == 0:
-            for _ in range(len(arr)):
-                if checkIfWorks(arr, time):
-                    print("I found the time: ", time)
-                    return
-                time += travel_by
+def partTwo(arr, time):
+    myBool = True
+    while myBool:
+        if time % int(arr[23]) == 0:
+            print("Checking this time ==> ", time)
+            if check(arr, time):
+                print("check this one out ", time)
+                return time
         time += 1
-
-        
-
 
 with open('data.txt') as raw_input:
     busses = []
@@ -45,10 +31,8 @@ with open('data.txt') as raw_input:
     for i in raw_input:
         for j in i:
             if j == ',':
-                # print(temp)
                 busses.append(temp)
                 temp = ""
             elif j.isnumeric():
                 temp += j 
-    print("DONE COLLECTING:", busses) 
-    print(partTwo(busses))
+    print(partTwo(busses, time))

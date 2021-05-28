06cba1187bb1fbe5359d0ef519cc9a50245fc20f
Hector-bit <hectormartinez.2978@gmail.com>
2021-05-28 02:17:01 -0700

    update to day 14, very long and tidious

--- day14/dockingData.py
+++ day14/dockingData.py
@@ -1,10 +1,17 @@
 
 
-def mask_operation(mask):
-    print('nothing')
 
-def mem_operation(memory):
-    print('nothing')
+def mem_operation(bitmask, memory):
+    print(bitmask, memory)
+    loopMe = bin(int(memory))[2:]
+    print(loopMe, "<== here")
+    for i in range(len(loopMe), -1, -1):
+        if bitmask[i] == 'X':
+            bitArray = list(bitmask)
+            print(bitArray, "<== should be an array")
+            bitArray[35 - i] = loopMe[len(loopMe) - 1]
+            bitmask = "".join(bitArray)
+    return bitmask
 
 def add_up_the_array(arr):
     adder = 0
@@ -21,12 +28,14 @@ with open('test.txt') as raw_intput:
         splitted = i.split('=')
         if splitted[0] == "mask ":
             current_mask = splitted[1]
-            print("new mask ==>", current_mask)
         else:
             #so here we get a change in the memory
             #so this piece of code needs to 
                 #change the mem using the mask
                 #and then add it back to the sum_of_arrays
                 #but if its all ready there then it needs to be replaced
-            print(splitted[0].split('['))
+            myElse = splitted[0].split('[')
+            memory = int(myElse[1].replace("] ", ""))
+            print(memory, type(memory))
+            print(mem_operation(current_mask[1: -1], splitted[1]))
     print(sum_of_arrays)

866a3b43f1527496d27c7bbe3e2dc163056d4158
Hector-bit <hectormartinez.2978@gmail.com>
2021-06-05 04:00:10 -0700

    code is a lot faster, a lot faster, but im too tired to clean it up, either way day 15 is done

--- day15/rambunctiousRicitation.py
+++ day15/rambunctiousRicitation.py
@@ -1,25 +1,34 @@
-test = [0,3,6]
-data = [9,19,1,6,0,5,4]
+test = [0,3]
+data = [9,19,1,6,0,5]
+
 def rr(n, arr):
     turn = len(arr)
+    smart_dict = {}
+    for index, i in enumerate(arr):
+        smart_dict[i] = index
+    #create a new num from the previous input
+    num = 4
     while turn != n:
-        #create a new num from the previous input
-        num = arr[-1]
         # print(turn, num)
         #do a quick check to see if the nuber exists
         #within the array, if it does then add a zero to arr
-        if num not in arr[:-1]:
+        if num not in smart_dict.keys():
             # print(num, 'not found so zero')
-            arr.append(0)
-        #here we loop backwards if the number isn't new
+            smart_dict[num] = turn
+            num = 0
+        #if the number is in the dictionary then we need
+        #to subtract the indexs of the most recent
         else:
+            temp = num - 0
+            num = turn - smart_dict[temp]
+            # print(num, 'FIRST SHOULD BE A THREE', temp)
+            smart_dict[temp] = turn
             # print('In arr is', num, arr)
-            for i in range(len(arr) - 2, -1, -1):
-                if arr[i] == num:
-                    arr.append(len(arr)-1-i)
-                    break
+            # for i in range(len(smart_dict) - 2, -1, -1):
+            #     if smart_dict.keys[i] == num:
+            #         smart_dict[len(arr)-1-i] = turn
+            #         break
         turn += 1
-    print(arr)
-    return arr[-1]
+    print(turn, num)
 
-print(rr(30000000, test))
+rr(30000000 - 1, data)

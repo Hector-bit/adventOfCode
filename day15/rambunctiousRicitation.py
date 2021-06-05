0c08c1953d7c5d18eed83c33b9cb1f322df51c50
Hector-bit <hectormartinez.2978@gmail.com>
2021-06-05 16:50:55 -0700

    cleaned up print statements

--- day15/rambunctiousRicitation.py
+++ day15/rambunctiousRicitation.py
@@ -1,3 +1,4 @@
+ 
 test = [0,3]
 data = [9,19,1,6,0,5]
 
@@ -9,11 +10,9 @@ def rr(n, arr):
     #create a new num from the previous input
     num = 4
     while turn != n:
-        # print(turn, num)
         #do a quick check to see if the nuber exists
         #within the array, if it does then add a zero to arr
         if num not in smart_dict.keys():
-            # print(num, 'not found so zero')
             smart_dict[num] = turn
             num = 0
         #if the number is in the dictionary then we need
@@ -21,13 +20,7 @@ def rr(n, arr):
         else:
             temp = num - 0
             num = turn - smart_dict[temp]
-            # print(num, 'FIRST SHOULD BE A THREE', temp)
             smart_dict[temp] = turn
-            # print('In arr is', num, arr)
-            # for i in range(len(smart_dict) - 2, -1, -1):
-            #     if smart_dict.keys[i] == num:
-            #         smart_dict[len(arr)-1-i] = turn
-            #         break
         turn += 1
     print(turn, num)
 

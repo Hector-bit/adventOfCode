aaf5c2a21a68850c5904a03750ab00911e5956f9
Hector-bit <hectormartinez.2978@gmail.com>
2021-06-05 02:58:01 -0700

    checkpoint

--- day15/rambunctiousRicitation.py
+++ day15/rambunctiousRicitation.py
@@ -2,14 +2,23 @@ test = [0,3,6]
 
 def rr(n, arr):
     turn = 0
-    while turn != 2020:
+    while turn != n:
         #create a new num from the previous input
         num = arr[-1]
-        for i in range(len(test), -1, -1):
-            if test[i] == num:
-                arr.append(len(test) -1 -i)
-        
+        # print(turn, num)
+        #do a quick check to see if the nuber exists
+        #within the array, if it does then add a zero to arr
+        if num not in arr[:-1]:
+            # print(num, 'not found so zero')
+            arr.append(0)
+        #here we loop backwards
+        else:
+            # print('In arr is', num, arr)
+            for i in range(len(arr) - 2, -1, -1):
+                if arr[i] == num:
+                    arr.append( - 1 -i)
         turn += 1
-    return turn
+    print(arr)
+    return arr[-1]
 
-print(rr(2020, test))
+print(rr(10, test))

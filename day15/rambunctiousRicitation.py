a23f774c2fbfffcbe2e3280fcb35137daba2f3d7
Hector-bit <hectormartinez.2978@gmail.com>
2021-06-05 03:05:04 -0700

    part one done

--- day15/rambunctiousRicitation.py
+++ day15/rambunctiousRicitation.py
@@ -1,7 +1,7 @@
 test = [0,3,6]
-
+data = [9,19,1,6,0,5,4]
 def rr(n, arr):
-    turn = 0
+    turn = len(arr)
     while turn != n:
         #create a new num from the previous input
         num = arr[-1]
@@ -11,14 +11,15 @@ def rr(n, arr):
         if num not in arr[:-1]:
             # print(num, 'not found so zero')
             arr.append(0)
-        #here we loop backwards
+        #here we loop backwards if the number isn't new
         else:
             # print('In arr is', num, arr)
             for i in range(len(arr) - 2, -1, -1):
                 if arr[i] == num:
-                    arr.append( - 1 -i)
+                    arr.append(len(arr)-1-i)
+                    break
         turn += 1
     print(arr)
     return arr[-1]
 
-print(rr(10, test))
+print(rr(30000000, test))

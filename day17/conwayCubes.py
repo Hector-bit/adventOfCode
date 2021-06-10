0cc58c013ac5ecfc6ca45882b6a5b75c35978ec1
Hector-bit <hectormartinez.2978@gmail.com>
2021-06-10 01:03:31 -0700

    had to get help, but i got part one done

--- day17/conwayCubes.py
+++ day17/conwayCubes.py
@@ -1,90 +1,48 @@
-def countAlivePoints(dictionary):
-    thingy = 0
-    print(dictionary)
-    for i in dictionary.values():
-        if i == 'ACTIVE':
-            thingy += 1
-    return thingy
 
-def addboxes(book, boxMethod):
-    add = []
-    prev_point = (0,0,0)
-    for i in book.keys():
-        for check in boxMethod:
-            if check not in book.keys():
-                add.append(check)
-        curr_point = i
-        #adjust the box as needed
-        x = curr_point[0] - prev_point[0]
-        y = curr_point[1] - prev_point[1]
-        z = curr_point[2] - prev_point[2]
-        for index, num in enumerate(boxMethod):
-            rx = num[0] + x
-            ry = num[1] + y
-            rz = num[2] + z
-            boxMethod[index] = (rx, ry, rz)
-        prev_point = i
-    for thing in add:
-        book[thing] = "INACTIVE"
 
-def cycle(cycles, dict):
-    #go through the dictionary and apply the rules
-    for _ in range(cycles):
-        boxMethod = [(1,0,0),(1,1,0),(0,1,0),(-1,1,0),(-1,0,0),(-1,-1,0),(0,-1,0),(1,-1,0),(1,0,-1),(1,1,-1),(0,1,-1),(-1,1,-1),(-1,0,-1),(-1,-1,-1),(0,-1,-1),(1,-1,-1),(1,0,1),(1,1,1),(0,1,1),(-1,1,1),(-1,0,1),(-1,-1,1),(0,-1,1),(1,-1,1),(0,0,1),(0,0,-1)]
-        prev_point = (0,0,0)
-        destroyPoint = []
-        activatePoint = []
-        #make sure all the boxMethods points are in the dictionary
-        addboxes(dict, boxMethod.copy())
-        print(boxMethod)
-        for cb in dict.keys():
-            curr_point = cb
-            #adjust the box as needed
-            x = curr_point[0] - prev_point[0]
-            y = curr_point[1] - prev_point[1]
-            z = curr_point[2] - prev_point[2]
-            for index, num in enumerate(boxMethod):
-                rx = num[0] + x
-                ry = num[1] + y
-                rz = num[2] + z
-                boxMethod[index] = (rx, ry, rz)
-            # print(boxMethod, '/n/n')
-            #check neighbors
-            aliveNeighbors = 0
-            for neighbor in boxMethod:
-                if neighbor in dict.keys():
-                    if dict[neighbor] == "ACTIVE":
-                        aliveNeighbors += 1
-            #check if point should become alive or dead
-            if dict[cb] == "ACTIVE":
-                if aliveNeighbors == 2 or aliveNeighbors == 3:
-                    continue
+def getNeighbors(someSet):
+    # print(someSet, '<========')
+    x, y, z = someSet
+    # print(x, y, z, 'and then', someSet)
+    newSet = set()
+    for xdel in range(-1,2):
+        for ydel in range(-1,2):
+            for zdel in range(-1,2):
+                neigh = (x + xdel, y + ydel, z + zdel)
+                newSet.add(neigh)
+    newSet.remove(someSet)
+    return newSet
+
+def stepSimulation(activeNodes):
+    newNodes = set()
+    activationCounts = {}
+    for i in activeNodes:
+        activeNeighbors = 0
+        neighbors = getNeighbors(i)
+        # print(neighbors)
+        for n in neighbors:
+            if n in activeNodes:
+                activeNeighbors += 1
+            else:
+                if n not in activationCounts.keys():
+                    activationCounts[n] = 1
                 else:
-                    destroyPoint.append(cb)
-            elif dict[cb] == "INACTIVE":
-                if aliveNeighbors == 3:
-                    activatePoint.append(cb)
-            prev_point = cb
-        print(destroyPoint, activatePoint)
-        for a in activatePoint:
-            dict[a] = "ACTIVE"
-        for d in destroyPoint:
-            dict[d] = "INACTIVE"
-        # dict[(0,0,0)] = "BITCHES"
-    return countAlivePoints(dict)
+                    activationCounts[n] += 1
+        if activeNeighbors == 2 or activeNeighbors == 3:
+            newNodes.add(i)
+    for potential in activationCounts:
+        if activationCounts[potential] == 3:
+            newNodes.add(potential)
+    # print(activationCounts)
+    return newNodes
 
-with open('test.txt') as raw_input:
-    #going to keep track of seen points in a dictionary(loving dictionarys rn, so quick)
-    smart_dict = {}
-    #you read that data as (x, y, 0)
-    #z in the data is always zero
+with open('data.txt') as raw_input:
+    mySet = set()
     for indexI, i in enumerate(raw_input):
         for indexJ, j in enumerate(i):
-            #we want add all these points to the smart_dict
-            #key is an array[x,y,z]
-            if j == '#':
-                smart_dict[(indexI, indexJ, 0)] = "ACTIVE"
-            elif j == '.':
-                smart_dict[(indexI, indexJ, 0)] = "INACTIVE"
-    #at this point we can now cycle 6 times using our dictionary
-    print(cycle(6, smart_dict))
+            if j == "#":
+                mySet.add((indexJ, indexI, 0))
+    state = mySet
+    for i in range(6):
+        state = stepSimulation(state)
+    print(len(state))
---
+++ day17/solFromgithubp1.py
@@ -0,0 +1,22 @@
+import fileinput 
+import re 
+import itertools
+
+
+def solve(p1, cycles):
+    ON = set()
+    L = list([l.strip() for l in fileinput.input('test.txt')])
+    for index, l in enumerate(L):
+        for indexCH, ch in enumerate(l):
+            if ch == '#':
+                ON.add((index,indexCH,0,0))
+    for _ in range(cycles):
+        NEW_ON = set()
+        CHECK = set()
+        for (x,y,z,w) in ON:
+            for dx, dy, dz, dw in itertools.product([-1,0,1], repeat=4):
+                if w+dw==0 or (not p1):
+                    CHECK.add((x+dx, y+dy, z+dz, w,dw))
+
+print(solve(True))
+

5aa87d47b00ece16480c1d49a5efb5529399a82f
Hector-bit <hectormartinez.2978@gmail.com>
2021-06-12 00:12:11 -0700

    save

--- day18/operationOrder.py
+++ day18/operationOrder.py
@@ -1,5 +1,25 @@
+def math(str):
+    print(str)
+
 def recurse(str):
-    for i in enumerate
+    #this takes a line of math the returns a number
+    num = None
+    for index, i in enumerate(str):
+        if i == '(':
+            remaining = (str[index+1:])
+            for ind, j in enumerate(remaining):
+                if j == '(':
+                    print('RECURSE')
+                    recurse(remaining)
+                elif j == ')':
+                    #do some math for the line
+                    #a number is returned
+                    print('found end')
+                else:
+                    splitted = remaining.split(' ')
+                    print(splitted)
+    return num
+
 
 
 with open('test.txt') as raw_input:

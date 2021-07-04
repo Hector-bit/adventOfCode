c193dc03e4f386768c7a1108b77cdabb7a85365b
Hector-bit <hectormartinez.2978@gmail.com>
2021-07-04 16:03:35 -0700

    cleaned up comments

--- day21/allergenAssessment.py
+++ day21/allergenAssessment.py
@@ -6,22 +6,13 @@ final = {}
 
 partTwo = []
 twoDict = {}
-empty = {}
 
 with open('data.txt') as raw_input:
     for i in raw_input:
-        # print(i)
         splitted = i.split("(")
-        # print(splitted)
-        # print(str(splitted[0]))
         language = splitted[0][:-1].split(" ")
-        # print(language)
-        # print(mylist)
-        # if language not in mylist:
         mylist.append(language)
         allergens = splitted[1][:-2].replace("contains ", "").split(", ")
-        # print(allergens)
-        # print(allergens)
         for j in allergens:
             if j not in mydict.keys():
                 mydict[j] = language
@@ -31,10 +22,9 @@ with open('data.txt') as raw_input:
                 save = []
                 for copy in temp:
                     if copy in language:
-                        # print(language, 'then', copy)
                         save.append(copy)
                 mydict[j] = save
-    # print(mydict)
+
     while len(mydict) > 0:
         #loops through keys of mydict
         for i in mydict.keys():
@@ -46,17 +36,14 @@ with open('data.txt') as raw_input:
 
                 break
             #else go throught the possible matches and look for repeats in final
-            # there is more than one word
+            #there is more than one word
             for search in mydict[i]:
-                # print('Searching: ', search, "In list: ", final)
                 if search in final:
                     mydict[i].remove(str(search))
-        # print(final,'final') 
-        # print(mydict, 'result')
+
     notallergens = 0
     for counting in mylist:
         for oneMore in counting:
-            # print(oneMore)
             if oneMore not in final:
                 notallergens +=1
     
@@ -65,16 +52,9 @@ with open('data.txt') as raw_input:
     
     for en in twoDict.keys():
         partTwo.append(en)
-
     partTwo.sort()
-    print(partTwo, twoDict)
     for index, k in enumerate(partTwo):
-        # print(index, k)
         partTwo[index] = twoDict[k]
-
-
-    print(partTwo)
-
 
     print("Part One", notallergens)
     print("Part Two", ','.join(partTwo))

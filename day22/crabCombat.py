6682b1a36d63d4aa315746a0d4d4505f39e57434
Hector-bit <hectormartinez.2978@gmail.com>
2021-07-04 23:20:15 -0700

    a bit messy(ill fix it later) but works fairly well

--- day22/crabCombat.py
+++ day22/crabCombat.py
@@ -5,23 +5,59 @@ playerTwo = []
 
 mylist = []
 
-with open('test.txt') as raw_input:
+with open('data.txt') as raw_input:
     for i in raw_input:
         if i[:-1].isdigit():
             mylist.append(i[:-1])
     playerOne = mylist[:len(mylist)>>1]
     playerTwo = mylist[len(mylist)>>1:]
-    print(playerOne)
-    print(playerTwo)
+    # print(playerOne)
+    # print(playerTwo)
     #now you may play until a player has lost all their cards
-    while len(playerTwo) <= 0 or len(playerOne) <= 0:
-        po = playerOne[0]
-        pt = playerTwo[0]
+    round = 1
+    while len(playerTwo) > 0 or len(playerOne) > 0:
+        if len(playerTwo) == 0 or len(playerOne) == 0:
+            break
+        # print("====", round, "====")
+        # print("PLAYER ONE: ",playerOne, len(playerOne))
+        # print("PLAYER TWO: ",playerTwo, len(playerTwo))
+        po = int(playerOne[0])
+        playerOne.pop(0)
+        pt = int(playerTwo[0])
+        playerTwo.pop(0)
         if po > pt:
             playerOne.append(po)
             playerOne.append(pt)
-            playerOne.pop(0)
+            # playerTwo.pop(0)
+            # playerOne.pop(0)
         elif pt > po:
             playerTwo.append(pt)
             playerTwo.append(po)
-            playerTwo.pop(0)
+            # playerOne.pop(0)
+            # playerTwo.pop(0)
+        # elif po > pt and len(playerTwo) == 1:
+        #     playerOne.append(po)
+        #     playerOne.append(pt)
+        #     playerTwo.pop(0)
+        #     playerOne.pop(0)
+        #     # break
+        # elif pt > po and len(playerOne) == 1:
+        #     playerTwo.append(pt)
+        #     playerTwo.append(po)
+        #     playerOne.pop(0)
+        #     playerTwo.pop(0)
+        #     # break
+        round += 1
+    print("====FINAL====")
+    print(playerOne)
+    print(playerTwo)
+    sum = len(playerTwo)
+    total = 0
+    sum2 = len(playerOne)
+    total2 = 0
+    for index, i in enumerate(playerTwo):
+        total += (sum - index) * i
+    for indext, it in enumerate(playerOne):
+        total2 += (sum2 - indext) * it
+    print(total)
+    print(total2)

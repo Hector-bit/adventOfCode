f8f4980aa7a9d147de054a721249d69de08d9145
Hector-bit <hectormartinez.2978@gmail.com>
2021-07-23 01:45:53 -0700

    part one is done

---
+++ day23/crabCups.py
@@ -0,0 +1,37 @@
+
+test = [3,8,9,1,2,5,4,6,7]
+data = [7,1,2,6,4,3,5,8,9]
+    #Rules
+        #1) the crab picks up the three cups that are after the current cup
+        #2) selecting a destingation cup: the cup with a label equal to currentCup - 1
+            #if one is not found then subtract 1 until it hits 0, then find largest label available
+        #3) the crab places the cups it picked up down after the destination cup has been chosen,
+            # they keep the same order
+        #4) the crab selcts a new current cup, which is whatever comes after 1 spot going clockwise
+def crabDancer(arr, x):
+    currentCup = 0
+    for i in range(x):
+        following = [arr[currentCup+i] for i in range(1,4)]
+        target = arr[currentCup] - 1
+        result = arr[4:]
+        destinationCup = None
+        # print(following, target, result)
+        while destinationCup is None:
+            if target == 0:
+                destinationCup = max(result)
+            for i in result:
+                if i == target:
+                    destinationCup = i
+            target -=1
+        # print(destinationCup)
+        for index, j in enumerate(result):
+            if j == destinationCup:
+                newArr = result[:index+1] + following + result[index+1:]
+                newArr.append(arr[currentCup])
+                # print(newArr)
+        arr = newArr
+        currentCup 
+    print(arr)
+
+
+crabDancer(data, 100)
--- day23/notes.md
+++ day23/notes.md
@@ -1,3 +1,9 @@
 Day 23:
 	Part One:
+		The crab plays a game where he manipulates a circle of numbers
+		After he does his rules on the numbers for x amount of times
+		I am suppose to return a string of numbers going clockwise from 1 (excluding the number 1)
+
+		Not so bad, but that only means that part 2 is going to kick my butt
+	Part Two:
 		

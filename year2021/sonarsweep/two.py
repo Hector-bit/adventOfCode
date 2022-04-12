a65a1fa2fd491dcd490e9930f3d984625bab068a
Hector-bit <hectormartinez.2978@gmail.com>
2022-04-12 03:20:46 -0700

    done with day 1

--- year2021/sonarsweep/two.py
+++ year2021/sonarsweep/two.py
@@ -1,22 +1,17 @@
 
-with open("test.txt") as raw_input:
+with open("data.txt") as raw_input:
     prev = None
     increases = 0
     windows = []
     #first loop will create the sum of 3 numbers to create a window
-    a = None
-    b = None
+    a = 0
+    b = 0
     for index, _ in enumerate(raw_input):
         c = int(_)
-        print(c, " index: ", index)
-        if (index % 3 == 0):
-            a = 
-        if (index % 3 == 1):
-            b = int(c)
         if index >= 2:
-            print("a: ", a, " b: ", b, " c: ", c)
             windows.append(a+b+c)
-    print("windows", windows)
+        a = b
+        b = c
 #copied over the part one code and subbed in the windows array
     for i in windows:
         curr = int(i)
